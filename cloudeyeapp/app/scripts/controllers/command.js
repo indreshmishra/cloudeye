6bb97d33fcec3f17d6fa2b2700d334de0d4c5e53
indresh mishra <rwork.droplate@gmail.com>
2016-11-11 11:54:12 +0530

    added creating command view in UI

--- cloudeyeapp/app/scripts/controllers/command.js
+++ cloudeyeapp/app/scripts/controllers/command.js
@@ -22,4 +22,19 @@ angular.module('cloudeyeappApp')
 
       });
       };
+
+
+$scope.newcommand=function(){
+      console.log('creating new command');
+      $http.post('http://localhost:8080/command' ,$scope.command,{headers:{ 'Authorization': 'Basic '+$cookieStore.get('globals').currentUser.authdata}}).success(function(data,status){
+              console.log('command is created');
+               $scope.command={};
+              $http.get('http://localhost:8080/command/',{headers:{ 'Authorization': 'Basic '+$cookieStore.get('globals').currentUser.authdata}})
+                    .success(function(response){
+                      $scope.commands= response;
+                    });
+
+            });
+      };
+
  });
